b8fe771f4ae9c60a85cd88385e59ebdfb799d198
wpollek <wpollek@umail.ucsb.edu>
2017-06-04 22:56:55 -0700

    testing push/pull  with comment

--- app/src/main/java/edu/ucsb/cs/cs185/npoon/mydog/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/npoon/mydog/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+//home should have swipe stack built in
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
