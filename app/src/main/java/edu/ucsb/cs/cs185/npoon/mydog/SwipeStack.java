04ebe679fbb97989898c138fa5919f5c77c557c7
Yamen Alghrer <yamenghrer@gmail.com>
2017-06-08 21:05:28 -0700

    initial screen basic set up - going to implement swipestack for seeking a pet

---
+++ app/src/main/java/edu/ucsb/cs/cs185/npoon/mydog/SwipeStack.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs.cs185.npoon.mydog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwipeStack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_stack);
+    }
+}
