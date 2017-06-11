3a8dac975f3eec880c59dacc80bb6a38f20a23c3
nickkpoon <nickkpoon@gmail.com>
2017-06-11 00:37:47 -0700

    fixed login textboxes + more changes to login screen/splash screen/home screen

--- app/src/main/java/edu/ucsb/cs/cs185/npoon/mydog/LoginActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/npoon/mydog/LoginActivity.java
@@ -1,12 +1,16 @@
 package edu.ucsb.cs.cs185.npoon.mydog;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,8 +19,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText et_password;
     TextView tv_create_acc;
     TextView tv_forgot_pass;
-    TextView tv_continue;
     Button btn_login;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +29,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         et_username = (EditText) findViewById(R.id.et_login_username);
         et_password = (EditText) findViewById(R.id.et_login_password);
-        tv_continue = (TextView) findViewById(R.id.tv_continue);
         tv_create_acc = (TextView) findViewById(R.id.tv_create_acc);
         tv_forgot_pass = (TextView) findViewById(R.id.tv_forgot_pass);
         btn_login = (Button) findViewById(R.id.btn_login);
+        logo = (ImageView) findViewById(R.id.login_logo);
+
+        et_username.setSingleLine(false);
+        et_username.setImeOptions(EditorInfo.IME_FLAG_NO_ENTER_ACTION);
+        et_username.setTextColor(getResources().getColor(R.color.primary_text));
+
+
+        et_password.setSingleLine(false);
+        et_password.setImeOptions(EditorInfo.IME_FLAG_NO_ENTER_ACTION);
+        et_password.setTextColor(getResources().getColor(R.color.primary_text));
+
+
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,13 +73,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        tv_continue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
 
     }
 
