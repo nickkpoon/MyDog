b8c502a225e6519517e36fcff92ec80c097a0ed2
Yamen Alghrer <yamenghrer@gmail.com>
2017-06-12 03:28:30 -0700

    Added example dogs for the swipestack

--- app/src/main/java/edu/ucsb/cs/cs185/npoon/mydog/PetSwipeActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/npoon/mydog/PetSwipeActivity.java
@@ -4,6 +4,7 @@ import android.media.Image;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -11,6 +12,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,11 +23,34 @@ public class PetSwipeActivity extends AppCompatActivity implements link.fls.swip
     public static ArrayList<PetObject> mPets = new ArrayList<>();
     private SwipeStack swipeStack;
 
+    //Dummy Data for Slideshow
+
+    PetObject a = new PetObject(Uri.parse("android.resource://edu.ucsb.cs.cs185.npoon.mydog/drawable/" + R.drawable.pug),
+            "Rex", "Pug", "He's friendly", 2, 1111111111, '5');
+    PetObject b = new PetObject(Uri.parse("android.resource://edu.ucsb.cs.cs185.npoon.mydog/drawable/" + R.drawable.corgy),
+            "Bailey", "Corgy", "Very Engergetic", 1, 2123314512, '5');
+    PetObject c = new PetObject(Uri.parse("android.resource://edu.ucsb.cs.cs185.npoon.mydog/drawable/" + R.drawable.lab),
+            "Charlie", "Lab", "Loves cuddling!", 4, 1472927821, '5');
+    PetObject d = new PetObject(Uri.parse("android.resource://edu.ucsb.cs.cs185.npoon.mydog/drawable/" + R.drawable.husky),
+            "Buddy", "Husky", "Really Independent", 3, 1232222222, '5');
+    PetObject e = new PetObject(Uri.parse("android.resource://edu.ucsb.cs.cs185.npoon.mydog/drawable/" + R.drawable.bull),
+            "Rosie", "Bull", "Always wants to play catch", 6, 1123456784, '5');
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_stack);
+
+
+
         //dummy data just to test
+        mPets.add(a);
+        mPets.add(b);
+        mPets.add(c);
+        mPets.add(d);
+        mPets.add(e);
         swipeStack = (SwipeStack) findViewById(R.id.swipeStack);
         swipeStack.setAdapter(new SwipeStackAdapter(mPets));
 
