3151e12130d7d702f2909a3a8d4dba37235384d9
nickkpoon <nickkpoon@gmail.com>
2017-06-12 04:08:24 -0700

    splash and home screen final

--- app/src/main/java/edu/ucsb/cs/cs185/npoon/mydog/PetSwipeActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/npoon/mydog/PetSwipeActivity.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs.cs185.npoon.mydog;
 
 import android.media.Image;
 import android.net.Uri;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -42,6 +43,9 @@ public class PetSwipeActivity extends AppCompatActivity implements link.fls.swip
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_stack);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
 
 
 
